25ea03b361791c107b54f442c3cad876c1926f26
DEMUK_ <denysmutay@gmail.com>
2021-04-08 07:57:18 +0300

    finding an element using individual html tag and using the dind_all() method and find() for the first instance

--- wscrapper.py
+++ wscrapper.py
@@ -15,5 +15,38 @@ soup = BeautifulSoup(response.content, 'html.parser')
 # print(list(soup.children))
 
 # loop through the children tags and print them out
-print([type(item) for item in list(soup.children)])
+# print([type(item) for item in list(soup.children)])
 
+# select the html tag by taking the third item in the list
+
+html = list(soup.children)[2]
+# print (html)
+
+# find the children inside the html tag
+
+# print(list(html.children))
+
+# get the p tag by finding children of the body tag
+
+body = list(html.children)[3]
+
+# print(body)
+
+list(body.children)
+
+p = list(body.children)[1]
+
+# print(p)
+
+p_text = p.get_text()
+
+# print(p_text)
+
+
+# using the find_all method to find instances on a page
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+all_p_instances = soup.find_all('p')
+
+# print(all_p_instances)
