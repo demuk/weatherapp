5e5fc06649c4a420e94d745f3663ddc5ddfdcd4e
DEMUK_ <denysmutay@gmail.com>
2021-04-08 07:06:41 +0300

    added loop to loop through the list of children tags instide html file

--- wscrapper.py
+++ wscrapper.py
@@ -1,5 +1,19 @@
 import requests
 
+from bs4 import BeautifulSoup
+
 response = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
 
-print(response.status_code)
+# print(response.status_code)
+
+# print(response.content)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# print(soup.prettify())
+
+# print(list(soup.children))
+
+# loop through the children tags and print them out
+print([type(item) for item in list(soup.children)])
+
