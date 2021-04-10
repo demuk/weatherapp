05f3b78531612def6c7de12b3eb9f99864099cbf
DEMUK_ <denysmutay@gmail.com>
2021-04-10 12:13:29 +0300

    update to the module

--- googlehpage.py
+++ googlehpage.py
@@ -7,3 +7,28 @@ status_codes = page.status_code
 
 print(status_codes)
 
+# print(page.headers)
+
+contents = page.content
+
+# print(contents)
+
+soups = BeautifulSoup(contents, 'html.parser')
+
+# print(soups.prettify())
+
+
+all_links = soups.find_all('a')
+
+# print(all_links)
+
+all_images = soups.find_all('img')
+
+# print(all_images)
+
+
+for links in all_links:
+    if "Hifadhi" in links.text:
+        print(links.attrs['href'])
+        
+
