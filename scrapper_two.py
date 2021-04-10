c3bbe8c76ed9d961f9c7f713c59ceff2673529c3
DEMUK_ <denysmutay@gmail.com>
2021-04-10 08:27:30 +0300

    using find() and find_all()

--- scrapper_two.py
+++ scrapper_two.py
@@ -3,4 +3,16 @@ from bs4 import BeautifulSoup
 
 page = requests.get("http://dataquestio.github.io/web-scraping-pages/ids_and_classes.html")
 
-print(page)
+print(page.status_code)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# print(soup)
+
+# print(soup.find_all('p', class_='outer-text'))
+
+# print(soup.find(id='first'))
+
+print(soup.select('div p'))
+
+
